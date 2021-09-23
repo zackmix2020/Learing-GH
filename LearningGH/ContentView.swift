ddb673c86b4692c348d197b1af53b8f4b667358e
Zack <zackmix2020@gmail.com>
2021-09-22 18:34:25 -0700

    color

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .fontWeight(.bold)
+            .foregroundColor(Color.orange)
             .padding()
     }
 }
